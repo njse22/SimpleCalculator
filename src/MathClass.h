97c71cf8eb2e5a411210d507867dc2c5db9d75ec
njse22 <njse22@gmail.com>
2018-12-27 17:58:49 -0500

    'test commit 2'

---
+++ src/MathClass.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <climits>
+
+#define sum(a,b) ((a)+(b))
+#define sus(a,b) ((a)-(b))
+#define mul(a,b) ((a)*(b))
+#define mod(a,b) ((a)%(b))
+#define max(a,b) ((a)>(b) ? (a) : (b))
+#define min(a,b) ((a)<(b) ? (a) : (b))
+#define equ(a,b) ((a)==(b))
+#define isPrime(a) (a%2 != 0)
+#define isPair(a) (a%2 == 0)
+
+using namespace std; 
+
+class MathClass{
+
+    int exponencial(int a , int b){
+        int i =0;
+	    int temp =a;
+	    while(i < b-1){
+		    a = a*temp;
+		    i++;
+	    }
+	return a;
+    }
+
+    double division(int a , int b){
+        long long int inf = INT_MAX; 
+        if(b == 0)
+            return inf;
+        else 
+            return static_cast < double > (a/b) ; 
+
+    }
+
+
+
+
+
+}; 
