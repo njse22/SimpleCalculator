c6f323e7d1bccb5e83129ade512fc6f91c7bf9b7
njse22 <njse22@gmail.com>
2018-12-27 21:12:27 -0500

    corrections of general erros in the class App and simple change in visivility of class MathClass

--- src/App.cpp
+++ src/App.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "MathClass.h"
 
 using namespace std; 
 
+//double division(int a , int b); 
+
 int main(){
-    
+    MathClass m{}; 
+
     cout << "Welcom to SimpleMath.\n" 
          << "Choose one operation.\n"
          << "1 . add two numbers.\n"
@@ -18,141 +22,162 @@ int main(){
          << "8 . compare two numbers.\n"
          << "9 . to determine if one number is pair.\n"
          << "10. to determine if one number is not pair.\n"
-         << "11. to use exponencial operation.\n"
-         << "12. to use the factorial operation.\n"
+         << "11. to use the factorial operation.\n"
+         << "12. to use exponencial operation.\n"<< endl; 
          
         int option{0};
         cin >> option; 
         
         while(option != -1){
-            
-            switch(option){
-                case 1: 
+            int a{0};
+            int b{0};
+            if(option == 0){
+                cout<< "Choose one operation.\n"
+                    << "1 . add two numbers.\n"
+                    << "2 . subtract two numbers.\n"
+                    << "3 . multiply two numbers.\n"
+                    << "4 . divide two numbers.\n"
+                    << "5 . get modulo between two numbers.\n"
+                    << "6 . get minimum between two numbers.\n"
+                    << "7 . get maximum between two numbers.\n"
+                    << "8 . compare two numbers.\n"
+                    << "9 . to determine if one number is pair.\n"
+                    << "10. to determine if one number is not pair.\n"
+                    << "11. to use the factorial operation.\n"
+                    << "12. to use exponencial operation.\n"<< endl; 
+                    cin >> option; 
+            }
+           else if(option == 1){
                     cout << "write the first number: "<< endl; 
-                    int a{0};
+                    cin >> a ; 
+                    cout << "write the second number: " << endl; 
+                    cin >> b; 
+                    cout << "the result of "<< a << "+" << b << " is: " <<  sum(a,b)
+                    << "\ndo you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option; 
+                    
+                    
+            }else if (option == 2){
+                    cout << "write the first number: "<< endl; 
+                    cin >> a; 
+                    cout << "write the second number: " << endl;
+                    cin >> b; 
+                    cout << "the result of "<< a << "-" << b << " is: " <<  sus(a,b)
+                    << "\ndo you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option; 
+                    
+                    
+            }else if (option == 3){
+                    cout << "write the first number: "<< endl; 
                     cin >> a ; 
                     cout << "write the second number: " << endl;
-                    int b{0};
                     cin >> b; 
-                    cout << "the result of "<< a << "+" << b << " is: " <<  sum(a,b)<< endl;
-                    break;  
-                case 2: 
-                    cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number: " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the result of "<< a << "-" << b << " is: " <<  sus(a,b)<< endl;
-                    break; 
-                case 3: 
-                    cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number: " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the result of "<< a << "x" << b << " is: " <<  mul(a,b)<< endl;
-                    break;   
-                case 4: 
+                    cout << "the result of "<< a << "x" << b << " is: " <<  mul(a,b)
+                    << "\ndo you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option; 
+                    
+            }else if(option == 4){
                     cout << "WARNING: if the second number is equals to zero the result is 'infinite'\n"<< endl; 
                     cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
+                    cin >> a; 
                     cout << "write the second number: " << endl;
-                    int b{0};
                     cin >> b; 
-                    cout << "the result of "<< a << "/" << b << " is: " <<  division(a,b)<< endl;
-                    break;  
-                case 5:
+                    cout << "the result of "<< a << "/" << b << " is: " << m.division(a,b) 
+                    << "\ndo you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option; 
+                    
+            }else if(option == 5){
+
                     cout << "write the first number: "<< endl; 
-                    int a{0};
                     cin >> a ; 
                     cout << "write the second number: " << endl;
-                    int b{0};
                     cin >> b; 
-                    cout << "the result of "<< a << "%" << b << " is: " <<  mod(a,b)<< endl;
-                    break; 
-                case 6: 
+                    cout << "the result of "<< a << "%" << b << " is: " <<  mod(a,b)
+                    << "\n do you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option;         
+            }
+            else if(option == 6){
                     cout << "write the first number: "<< endl; 
-                    int a{0};
                     cin >> a ; 
                     cout << "write the second number: " << endl;
-                    int b{0};
                     cin >> b; 
-                    cout << "the minimun value between "<< a << " and " << b << "is: " << min(a,b) << endl;
-                    break;   
-                case 7:
-                    cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number: " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the maximum value between "<< a << " and " << b << "is: " << max(a,b) << endl;
-                    break;   
-                case 8: 
-                    cout << "this option determines if two numbers are equlas\n" << endl; 
-                    cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number: " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the numbers are equals ? " << equ(a,b) ? "YES" : "NO" << endl; 
-                    break;   
-                case 9: 
-                    cout << "write the number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "the number is pair ? " << isPair(a) ? "YES" : "NO" << endl; 
-                    break;   
-                case 10: 
-                    cout << "write the number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "the number is odd ? " << isInPair(a) ? "YES" : "NO" << endl; 
-                    break;   
-                case 11: 
-                    cout << "this option determines if two numbers are equlas\n" << endl; 
-                    cout << "write the first number: "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number: " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the numbers are equals ? " << equ(a,b) ? "YES" : "NO" << endl; 
-                    break;
-                case 12: 
-                    cout << "this option determines what is the result of a^b\n" << endl; 
-                    cout << "write the first number (a): "<< endl; 
-                    int a{0};
-                    cin >> a ; 
-                    cout << "write the second number(b): " << endl;
-                    int b{0};
-                    cin >> b; 
-                    cout << "the resul between "<< a << "^" << b << " is: " << max(a,b) << endl;
-                    break;
+                    cout << "the minimun value between "<< a << " and " << b << " is: " << min(a,b)
+                    << "\ndo you want continue whit the program ?.\n"
+                    << " (-1) for exit , (0) for continue" << endl;
+                    cin >> option; 
+            }
+            else if (option == 7){
+                cout << "write the first number: "<< endl; 
+                cin >> a ; 
+                cout << "write the second number: " << endl;
+                cin >> b; 
+                cout << "the maximum value between "<< a << " and " << b << " is: " << max(a,b) 
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
+            }
+            else if(option == 8){
+                cout << "this option determines if two numbers are equlas\n" << endl; 
+                cout << "write the first number: "<< endl; 
+                cin >> a ; 
+                cout << "write the second number: " << endl;
+                cin >> b; 
+                string r =equ(a,b) ? "YES" : "NO"; 
+                cout << "the numbers are equals ? " << r 
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
+
+            }else if (option == 9){
+                cout << "write the number: "<< endl; 
+                cin >> a ; 
+                string r = isPair(a) ? "YES" : "NO";
+                cout << "the number is pair ? " << r 
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
 
 
+            }       
+            else if (option == 10){
+                cout << "write the number: "<< endl; 
+                cin >> a ; 
+                string r = isInPair(a) ? "YES" : "NO"; 
+                cout << "the number is odd ? " << r 
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
 
+            } 
+            else if(option == 11){
+                cout << "this program calculate what is the factorial of one number\n" << endl; 
+                cout << "write the first number: "<< endl; 
+                cin >> a ; 
+                cout << "the factorial of: " << a << " is: " << m.factorial(a)
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
 
-
-
+            } 
+            else if(option == 12){
+                cout << "this option determines what is the result of a^b\n" << endl; 
+                cout << "write the first number (a): "<< endl; 
+                cin >> a ; 
+                cout << "write the second number(b): " << endl;
+                cin >> b; 
+                cout << "the resul between "<< a << "^" << b << " is: " << m.exponencial(a,b)
+                << "\ndo you want continue whit the program ?.\n"
+                << " (-1) for exit , (0) for continue" << endl;
+                cin >> option; 
 
             }
-
-
-
-
+            
         }
-        
-
-
-
-
-
-
 
     return 0;
 }
--- src/MathClass.h
+++ src/MathClass.h
@@ -14,33 +14,33 @@
 using namespace std; 
 
 class MathClass{
-
-    int exponencial(int a , int b){
-        int i =0;
-	    int temp =a;
-	    while(i < b-1){
-		    a = a*temp;
-		    i++;
-	    }
-	return a;
-    }
-
-    double division(int a , int b){
-        long long int inf = INT_MAX; 
-        if(b == 0)
-            return inf;
-        else 
-            return static_cast < double > (a/b) ; 
-
-    }
-
-    long long int factorial(int a){
-        int r = 1; 
-        for(int i = 1 ; i < a, i++){
-            r*=i; 
+    public:
+        int exponencial(int a , int b){
+            int i =0;
+            int temp =a;
+            while(i < b-1){
+                a = a*temp;
+                i++;
+            }
+        return a;
         }
-        return r; 
-    }
+
+        double division(int a , int b){
+            long long int inf = INT_MAX; 
+            if(b == 0)
+                return inf;
+            else 
+                return static_cast < double > (a/b) ; 
+
+        }
+
+        long long int factorial(int a){
+            int r = 1; 
+            for(int i = 1 ; i < a; i++){
+                r*=i; 
+            }
+            return r; 
+        }
 
 
 
